d4f0b5f417c521f35e2626114867d4e8ad06380c
Ghulam Qadir <connecttoqadir@gmail.com>
2023-10-14 18:56:09 -0700

    registration api done

--- backend/index.js
+++ backend/index.js
@@ -6,17 +6,12 @@ const cors = require("cors")
 app.use(cors())
 app.use(express.json())
 app.use('/api/notes',require('./routes/notes'))
+app.use('/api/auth',require('./routes/users'))
 
 
 mongoose.connect('mongodb+srv://qadir:qadir@edify.0i5koc5.mongodb.net/?retryWrites=true&w=majority').then(()=>{
     console.log("Connected!")
 })
-
-
-
-
-
-
 
 
 app.listen(5000,()=>{
